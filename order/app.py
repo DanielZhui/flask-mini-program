477b39236b2c47f2cc476b3ba6ec8eb908af512f
Wollens <zh2017728@163.com>
2020-03-29 10:03:54 +0800

    add url_for(链接管理器)

--- order/app.py
+++ order/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, url_for
 from index import route
 
 app = Flask(__name__)
 app.register_blueprint(route, url_prefix='/api')
 
+
 @app.route('/')
 def index():
-    return 'hello world'
+    url = url_for('index')
+    print(url)
+    return 'hello world', url
+
+@app.route('/home')
+def home():
+    url = url_for('home')
+    print(url)
+    return 'welcome to home'
 
 if __name__ == "__main__":
     app.run()
