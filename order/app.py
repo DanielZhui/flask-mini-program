ff8c18ed4b150741870499f40ac6154917cfa50e
Wollens <zh2017728@163.com>
2020-04-02 09:11:08 +0800

    test slack noti

--- order/app.py
+++ order/app.py
@@ -3,7 +3,6 @@ from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from web.controllers.index import route_index
 
-# flask 源码解析, 类集成 以及配置文件的加载
 class Application(Flask):
     def __init__(self, import_name):
         super(Application, self).__init__(import_name)
