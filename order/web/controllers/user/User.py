524a59a1645ff1ada6d459c80df71f9f9162d03f
Wollens <zh2017728@163.com>
2020-04-05 22:39:29 +0800

    complete index page
    complete login page

--- order/web/controllers/user/User.py
+++ order/web/controllers/user/User.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, jsonify
 
 route_user = Blueprint('user_page', __name__)
 
 @route_user.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return 'get request'
-    render_template('user/login.html')
+        return render_template('user/login.html')
+    resp = {'code': 200, 'msg': '登录成功', 'data': {}}
+    req = request.values
+    login_name = req['login_name'] if 'login_name' in req else ''
+    login_pwd = req['login_pwd'] if 'login_pwd' in req else ''
+    return jsonify(resp)
