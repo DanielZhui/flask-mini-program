d9d4782893b99734b0334ff1974b27f9bbe39469
Wollens <zh2017728@163.com>
2020-04-05 21:24:59 +0800

    app add template_folder

--- order/app.py
+++ order/app.py
@@ -1,13 +1,15 @@
+import os
 from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
 class Application(Flask):
-    def __init__(self, import_name):
-        super(Application, self).__init__(import_name)
+    def __init__(self, import_name, template_folder=None, root_path=None, static_path=None):
+        super(Application, self).__init__(import_name, template_folder=template_folder, root_path=root_path)
         self.config.from_pyfile('config/base_setting.py')
         db.init_app(self)
 
+BASE_DIR = os.getcwd()
 db = SQLAlchemy()
-app = Application(__name__)
+app = Application(__name__, template_folder=BASE_DIR + '/web/templates', root_path=BASE_DIR)
 manager = Manager(app)
