2cc4d3130fdd5171bbe794553a23f82837ac8a67
Wollens <zh2017728@163.com>
2020-04-15 23:08:17 +0800

    start edit userinfo page

--- order/web/controllers/account/Account.py
+++ order/web/controllers/account/Account.py
@@ -9,7 +9,6 @@ route_account = Blueprint('account_page', __name__)
 def index():
     resp_data = {}
     users = User.query.order_by(User.uid.desc()).all()
-    print(users)
     resp_data['users'] = users
     return ops_render('/account/index.html', resp_data)
 
@@ -19,7 +18,6 @@ def info():
     if request_method == 'GET':
         resp_data = {}
         request_args = request.args
-        print(request_args)
         uid = request_args['id'] if 'id' in request_args else ''
         if not uid:
             return redirect('/account/index')
@@ -27,9 +25,13 @@ def info():
         resp_data['user'] = user
         return ops_render('/account/info.html', resp_data)
 
-@route_account.route('/set')
+@route_account.route('/set', methods=['GET', 'POST'])
 def set():
-    pass
+    resp_data= {}
+    request_method = request.method
+    if request_method == 'GET':
+        return ops_render('/account/set.html')
+    return ops_render('/account/set.html', resp_data)     
 
 @route_account.route('/delete')
 def delete():
